7ed860827bb6a0287a855170a39c03a698302a7a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-23 14:17:00 +0200

    Refactor CartModal to have portal inside component

--- src/pages/Cart/CartModal.tsx
+++ src/pages/Cart/CartModal.tsx
@@ -1,26 +1,33 @@
 import { motion } from "framer-motion";
+import ModalPortal from "../../components/Modal/ModalPortal";
+import ModalWrapper from "../../components/Modal/ModalWrapper";
 
 type CartModalProps = {
-  toggleCart: React.MouseEventHandler<HTMLButtonElement>;
+  toggleCart: () => void;
+  showCart: boolean;
 };
 
-export default function CartModal({ toggleCart }: CartModalProps) {
+export default function CartModal({ toggleCart, showCart }: CartModalProps) {
   return (
-    <motion.div
-      key="modal"
-      initial={{ x: -1500 }}
-      animate={{ x: 0, transition: { duration: 0.3 } }}
-      exit={{ x: 1500, transition: { duration: 0.3 } }}
-      onClick={(e) => e.stopPropagation()}
-      className="flex-col items-center rounded-lg bg-white p-6 text-center shadow-lg"
-    >
-      <h1 className="mb-10 text-5xl font-black">Shopping cart</h1>
-      <button
-        className="w-full rounded-lg bg-red-700 py-3 text-lg text-white shadow-lg hover:bg-red-600"
-        onClick={toggleCart}
-      >
-        Close
-      </button>
-    </motion.div>
+    <ModalPortal toggleModal={toggleCart} showModal={showCart}>
+      <ModalWrapper handleCloseModal={toggleCart}>
+        <motion.div
+          key="modal"
+          initial={{ x: -1500 }}
+          animate={{ x: 0, transition: { duration: 0.3 } }}
+          exit={{ x: 1500, transition: { duration: 0.3 } }}
+          onClick={(e) => e.stopPropagation()}
+          className="flex-col items-center rounded-lg bg-white p-6 text-center shadow-lg"
+        >
+          <h1 className="mb-10 text-5xl font-black">Shopping cart</h1>
+          <button
+            className="w-full rounded-lg bg-red-700 py-3 text-lg text-white shadow-lg hover:bg-red-600"
+            onClick={toggleCart}
+          >
+            Close
+          </button>
+        </motion.div>
+      </ModalWrapper>
+    </ModalPortal>
   );
 }
