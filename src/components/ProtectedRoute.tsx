5cc084b01a060cb849519e12d98aec54a309a716
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-26 18:52:36 +0200

    Add ProtectedRoute component

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom";
+import { PropsWithChildren } from "react";
+
+type ProtectedRouteProps = {
+  isUserLoggedIn: boolean;
+  redirectPath?: string;
+};
+
+const ProtectedRoute = ({
+  isUserLoggedIn,
+  redirectPath = "/",
+  children,
+}: PropsWithChildren<ProtectedRouteProps>): JSX.Element => {
+  
+  if (isUserLoggedIn) {
+    return <Navigate to={redirectPath} replace />;
+  }
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
