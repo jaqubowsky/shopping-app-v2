24508977115458cd564be17bfbbf2cbce3ada07b
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-26 18:55:37 +0200

    Add new routes: login, reset, register, profile

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,76 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Layout from "./components/Layout";
 import Contact from "./pages/Contact";
-import Login from "./pages/Login";
+import Register from "./pages/Login/Register";
+import Login from "./pages/Login/Login";
+import { ThemeProvider } from "@material-tailwind/react";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import Profile from "./pages/Profile";
+import Reset from "./pages/Login/Reset";
+import { useState } from "react";
 
 function App() {
+  const [user] = useAuthState(auth);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, setClose] = useState(false);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
   const routes = createRoutesFromChildren(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="products" element={<Products />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="login" element={<Login />} />
+      <Route
+        path="login"
+        element={
+          <ProtectedRoute isUserLoggedIn={user ? true : false}>
+            <Login error={error} setClose={setClose} setError={setError} />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="login/reset-password"
+        element={
+          <ProtectedRoute isUserLoggedIn={user ? true : false}>
+            <Reset
+              error={error}
+              setClose={setClose}
+              setError={setError}
+              success={success}
+              setSuccess={setSuccess}
+            />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="register"
+        element={
+          <ProtectedRoute isUserLoggedIn={user ? true : false}>
+            <Register error={error} setClose={setClose} setError={setError} />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="profile"
+        element={
+          <ProtectedRoute isUserLoggedIn={user ? false : true}>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
     </Route>
   );
 
   const router = createBrowserRouter(routes);
 
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
