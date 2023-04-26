e72fe9cb03e5865a20b1de87f36de664ea75de50
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-26 22:43:48 +0200

    Add validationInfo

---
+++ src/pages/Login/validationInfo.ts
@@ -0,0 +1,16 @@
+export const validationInfo = {
+  required: {
+    password: "Password is required",
+    cpassword: "Matchin password is required",
+    email: "Email is required",
+    username: "Username is required",
+  },
+  notMatch: {
+    password: "You need to provide matching password!",
+    email: "Entered value does not match email format",
+  },
+  notLongEnough: {
+    password: "Password must have at least 8 characters",
+  },
+  undefined: "Undefined error",
+};
