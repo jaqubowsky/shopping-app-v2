208d65c7c0891147ca8871fcac2771339ae09495
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-26 18:54:25 +0200

    Add user icon visibility when logged with Google

--- src/pages/Login/UserIcon.tsx
+++ src/pages/Login/UserIcon.tsx
@@ -1,21 +1,42 @@
 import { BiUser } from "react-icons/bi";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase";
 
 type UserIconProps = {
   toggleLoginDropdown: () => void;
 };
 
-function UserIcon({
-  toggleLoginDropdown,
-}: UserIconProps) {
+function UserIcon({ toggleLoginDropdown }: UserIconProps) {
+  const [user] = useAuthState(auth);
+  const userPhoto = user?.photoURL;
+
   return (
     <div
-      className="relative cursor-pointer rounded-full bg-white p-4 text-center shadow-md"
+      tabIndex={0}
+      role="button"
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          toggleLoginDropdown();
+          e.stopPropagation();
+        }
+        return;
+      }}
+      className="relative flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-white p-4 text-center shadow-md"
       onClick={(e) => {
         toggleLoginDropdown();
         e.stopPropagation();
       }}
     >
-      <BiUser className="text-3xl" />
+      {userPhoto ? (
+        <img
+          role="presentation"
+          className="scale-105 rounded-full"
+          src={userPhoto}
+          alt="user"
+        />
+      ) : (
+        <BiUser className="text-3xl" />
+      )}
     </div>
   );
 }
