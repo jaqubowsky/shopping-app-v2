b7653d4028d7338d801bba0b189a1f11a3f0279d
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-26 18:50:49 +0200

    Fix cart icon styling

--- src/pages/Cart/CartIcon.tsx
+++ src/pages/Cart/CartIcon.tsx
@@ -7,7 +7,7 @@ type CartIconProps = {
 function CartIcon({ toggleCart }: CartIconProps) {
   return (
     <div onClick={toggleCart}>
-      <div className="cursor-pointer rounded-full bg-white p-4 text-center shadow-md">
+      <div className=" h-16 w-16 cursor-pointer rounded-full bg-white p-4 text-center shadow-md">
         <AiOutlineShoppingCart className="text-3xl" />
       </div>
     </div>
