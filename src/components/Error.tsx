30a867e482550a51aa4fae1c6cfe93419db4175a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-26 18:51:32 +0200

    Add Error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,16 @@
+import { motion } from "framer-motion";
+
+type ErrorProps = {
+  message: string;
+};
+
+export function Error({ message }: ErrorProps) {
+  return (
+    <motion.span
+      animate={{ x: [-10, 10, -10, 10, 0], transition: { duration: 0.25 } }}
+      className="mb-3 pl-2 text-red-900"
+    >
+      {message}
+    </motion.span>
+  );
+}
