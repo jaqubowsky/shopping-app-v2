604b2925993bb6940dde8cb51c0e2e60cf4e902b
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-29 22:12:49 +0200

    Add keyboard handling

--- src/pages/Cart/CartIcon.tsx
+++ src/pages/Cart/CartIcon.tsx
@@ -5,11 +5,23 @@ type CartIconProps = {
 };
 
 function CartIcon({ toggleCart }: CartIconProps) {
+  const handleEnterPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      toggleCart();
+      e.stopPropagation();
+    }
+    return;
+  };
+
   return (
-    <div onClick={toggleCart}>
-      <div className=" h-16 w-16 cursor-pointer rounded-full bg-white p-4 text-center shadow-md">
-        <AiOutlineShoppingCart className="text-3xl" />
-      </div>
+    <div
+      onClick={toggleCart}
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleEnterPress}
+      className="flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-white p-4 text-center shadow-md"
+    >
+      <AiOutlineShoppingCart className="text-3xl" />
     </div>
   );
 }
