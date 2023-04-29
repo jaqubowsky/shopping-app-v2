61a83a62aae82498c2e4b884ac2a28743990d74c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-04-29 22:10:16 +0200

    Add typing

--- src/utils/getErrorMessage.ts
+++ src/utils/getErrorMessage.ts
@@ -1,4 +1,4 @@
-export function getErrorMessage(error: unknown) {
+export function getErrorMessage(error: unknown): string {
   if (error instanceof Error) return error.message;
   return String(error);
 }
