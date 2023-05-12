2b0d20a722871686a35e1030c308a7a9a5447585
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-12 13:21:20 +0200

    Fix error throwing and api request

--- src/api/productsApi.ts
+++ src/api/productsApi.ts
@@ -2,9 +2,15 @@ import axios, { AxiosResponse } from "axios";
 import { Product } from "../types/product";
 
 const API_URL = "http://localhost:3001/api/";
+const ALL_URL = "http://localhost:3001/";
 
 const api = axios.create({
   baseURL: API_URL,
+  withCredentials: true,
+});
+
+const all = axios.create({
+  baseURL: ALL_URL,
   withCredentials: true,
 });
 
@@ -18,7 +24,7 @@ export const addProduct = async (formData: FormData) => {
   } catch (err: unknown) {
     if (axios.isAxiosError(err)) {
       // eslint-disable-next-line
-      throw new Error(err.request?.response);
+      throw new Error(err.response?.data);
     } else {
       throw new Error("Unexpected error");
     }
@@ -27,7 +33,7 @@ export const addProduct = async (formData: FormData) => {
 
 export const getProducts = async (): Promise<Product[]> => {
   try {
-    const response: AxiosResponse<Product[]> = await api.get("/allproducts");
+    const response: AxiosResponse<Product[]> = await all.get("/allproducts");
 
     const products = response.data;
 
@@ -35,7 +41,24 @@ export const getProducts = async (): Promise<Product[]> => {
   } catch (err: unknown) {
     if (axios.isAxiosError(err)) {
       // eslint-disable-next-line
-      throw new Error(err.request?.response);
+      throw new Error(err.response?.data);
+    } else {
+      throw new Error("Unexpected error");
+    }
+  }
+};
+
+export const getUserProducts = async (): Promise<Product[]> => {
+  try {
+    const response: AxiosResponse<Product[]> = await api.get("/products");
+
+    const products = response.data;
+
+    return products;
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      // eslint-disable-next-line
+      throw new Error(err.response?.data);
     } else {
       throw new Error("Unexpected error");
     }
