ea3b9aa6a4b320d67cc35a8e551f18e6421f1d0b
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-12 13:16:42 +0200

    Create AddProductIcon.tsx

---
+++ src/components/Layout/AddProductIcon.tsx
@@ -0,0 +1,26 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
+function AddProductIcon() {
+  const handleEnterPress = (e: React.KeyboardEvent<HTMLAnchorElement>) => {
+    if (e.key === "Enter") {
+      // Add product functionality
+      e.stopPropagation();
+    }
+    return;
+  };
+
+  return (
+    <Link
+      to="/add-product"
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleEnterPress}
+      className="flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-white p-4 text-center shadow-md"
+    >
+      <AiOutlinePlus className="text-3xl" />
+    </Link>
+  );
+}
+
+export default AddProductIcon;
