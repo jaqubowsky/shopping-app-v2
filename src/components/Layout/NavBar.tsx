3a78d78c53471e0fa6ac3a290575c0d3117ff69c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-12 13:19:26 +0200

    Restyle NavBar

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,41 +1,16 @@
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { PropsWithChildren } from "react";
 
 function NavBar({ children }: PropsWithChildren) {
 
-  const getActiveStyles = (isActive: boolean) => {
-    return isActive
-      ? "scale-105 border-b-2 border-b-white"
-      : "flex items-center justify-center gap-6 text-lg hover:scale-105 hover:border-b-2 hover:border-b-white";
-  };
-
   return (
-    <nav className="mx-auto flex w-11/12 flex-col items-center justify-between">
-      <Link className="mb-4 text-6xl font-black" to=".">
-        Fake<span className="text-white drop-shadow-md">Store</span>
-      </Link>
+    <nav className="mx-auto flex w-11/12 items-center justify-around">
+      <div className="flex items-center gap-6">
+        <Link className="flex text-5xl font-black" to=".">
+          Fake <span className="text-white drop-shadow-md">Store</span>
+        </Link>
+      </div>
       <div className="hover flex flex-col items-center justify-center gap-6 text-lg">
-        <section className="flex gap-6">
-          <NavLink
-            className={({ isActive }) => getActiveStyles(isActive)}
-            to="/"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => getActiveStyles(isActive)}
-            to="products"
-          >
-            Products
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => getActiveStyles(isActive)}
-            to="contact"
-          >
-            Contact
-          </NavLink>
-        </section>
-
         <section className="flex gap-6">{children}</section>
       </div>
     </nav>
