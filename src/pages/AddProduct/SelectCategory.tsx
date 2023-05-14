1d9aa5940b818e65448de3bf7d1bf8e7aa539fd2
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-14 20:22:50 +0200

    Add SelectCategory component

---
+++ src/pages/AddProduct/SelectCategory.tsx
@@ -0,0 +1,32 @@
+import { Controller } from "react-hook-form";
+import { Select, Option } from "@material-tailwind/react";
+
+export default function SelectCategory({ control }) {
+  return (
+    <Controller
+      control={control}
+      name="category"
+      rules={{ required: "Category is required." }}
+      render={({ field: { onChange, name } }) => (
+        <Select
+          label="Select category"
+          color="amber"
+          onChange={onChange}
+          name={name}
+        >
+          <Option value={undefined}>---</Option>
+          <Option value="Clothes">Clothes</Option>
+          <Option value="Electronics">Electronics</Option>
+          <Option value="Motorization">Motorization</Option>
+          <Option value="Home and garden">Home and garden</Option>
+          <Option value="Real estate">Real estate</Option>
+          <Option value="Education">Education</Option>
+          <Option value="For kids">For kids</Option>
+          <Option value="Animals">Animals</Option>
+          <Option value="Sport and hobby">Sport and hobby</Option>
+          <Option value="Heath and beauty">Health and beauty</Option>
+        </Select>
+      )}
+    />
+  );
+};
