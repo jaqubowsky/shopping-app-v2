ea8acca6840a063a01e0a375d6ad900c9830635c
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-17 22:54:31 +0200

    Add name, surname, phoneNumber and location

--- src/pages/Login/Register.tsx
+++ src/pages/Login/Register.tsx
@@ -9,6 +9,11 @@ import { useErrorBoundary } from "react-error-boundary";
 
 type Inputs = {
   e?: Event;
+  name: string;
+  surname: string;
+  location: string;
+  phoneNumber: string;
+
   username: string;
   email: string;
   password: string;
@@ -26,12 +31,20 @@ export default function Register() {
   const { showBoundary } = useErrorBoundary();
 
   const onSubmit: SubmitHandler<Inputs> = async (
-    { email, password, username },
+    { email, password, username, name, surname, location, phoneNumber },
     e
   ) => {
     e?.preventDefault();
     try {
-      const registerValues = { email, password, username };
+      const registerValues = {
+        email,
+        password,
+        username,
+        name,
+        surname,
+        location,
+        phoneNumber,
+      };
       await registerWithEmailAndPassword(registerValues);
     } catch (err) {
       showBoundary(err);
@@ -50,88 +63,158 @@ export default function Register() {
         onSubmit={onPromise(handleSubmit(onSubmit))}
         className="mb-2 mt-8 w-80 max-w-screen-lg sm:w-96"
       >
-        <div className="mb-4 flex flex-col gap-2">
-          <Input
-            type="text"
-            size="lg"
-            label="Username"
-            {...register("username", {
-              required: validationInfo.required.username,
-            })}
-          />
-          {errors.username && (
-            <ErrorMessage
-              message={
-                errors.username.message
-                  ? errors.username.message
-                  : validationInfo.undefined
-              }
+        <div className="mb-4 flex flex-row gap-2">
+          <div className="flex flex-col gap-3">
+            <Input
+              type="text"
+              size="lg"
+              label="Name"
+              {...register("name", {
+                required: validationInfo.required.username,
+              })}
             />
-          )}
-          <Input
-            type="email"
-            size="lg"
-            label="Email"
-            {...register("email", {
-              required: true,
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: validationInfo.notMatch.email,
-              },
-            })}
-          />
-          {errors.email && (
-            <ErrorMessage
-              message={
-                errors.email.message
-                  ? errors.email.message
-                  : validationInfo.undefined
-              }
-            />
-          )}
-          <Input
-            type="password"
-            size="lg"
-            label="Password"
-            {...register("password", {
-              required: validationInfo.required.password,
-              minLength: {
-                value: 8,
-                message: validationInfo.notLongEnough.password,
-              },
-            })}
-          />
-          {errors.password && (
-            <ErrorMessage
-              message={
-                errors.password.message
-                  ? errors.password.message
-                  : validationInfo.undefined
-              }
-            />
-          )}
-          <Input
-            type="password"
-            size="lg"
-            label="Verify password"
-            {...register("cpassword", {
-              required: validationInfo.required.cpassword,
-              validate: (val: string) => {
-                if (watch("password") != val) {
-                  return validationInfo.notMatch.password;
+            {errors.name && (
+              <ErrorMessage
+                message={
+                  errors.name.message
+                    ? errors.name.message
+                    : validationInfo.undefined
                 }
-              },
-            })}
-          />
-          {errors.cpassword && (
-            <ErrorMessage
-              message={
-                errors.cpassword.message
-                  ? errors.cpassword.message
-                  : validationInfo.undefined
-              }
+              />
+            )}
+            <Input
+              type="text"
+              size="lg"
+              label="Surname"
+              {...register("surname", {
+                required: validationInfo.required.surname,
+              })}
             />
-          )}
+            {errors.surname && (
+              <ErrorMessage
+                message={
+                  errors.surname.message
+                    ? errors.surname.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+            <Input
+              type="number"
+              size="lg"
+              label="Number"
+              {...register("phoneNumber", {})}
+            />
+            {errors.phoneNumber && (
+              <ErrorMessage
+                message={
+                  errors.phoneNumber.message
+                    ? errors.phoneNumber.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+            <Input
+              type="text"
+              size="lg"
+              label="Location"
+              {...register("location", {
+                required: validationInfo.required.surname,
+              })}
+            />
+            {errors.location && (
+              <ErrorMessage
+                message={
+                  errors.location.message
+                    ? errors.location.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <Input
+              type="text"
+              size="lg"
+              label="Username"
+              {...register("username", {
+                required: validationInfo.required.username,
+              })}
+            />
+            {errors.username && (
+              <ErrorMessage
+                message={
+                  errors.username.message
+                    ? errors.username.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+            <Input
+              type="email"
+              size="lg"
+              label="Email"
+              {...register("email", {
+                required: true,
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: validationInfo.notMatch.email,
+                },
+              })}
+            />
+            {errors.email && (
+              <ErrorMessage
+                message={
+                  errors.email.message
+                    ? errors.email.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+            <Input
+              type="password"
+              size="lg"
+              label="Password"
+              {...register("password", {
+                required: validationInfo.required.password,
+                minLength: {
+                  value: 8,
+                  message: validationInfo.notLongEnough.password,
+                },
+              })}
+            />
+            {errors.password && (
+              <ErrorMessage
+                message={
+                  errors.password.message
+                    ? errors.password.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+            <Input
+              type="password"
+              size="lg"
+              label="Verify password"
+              {...register("cpassword", {
+                required: validationInfo.required.cpassword,
+                validate: (val: string) => {
+                  if (watch("password") != val) {
+                    return validationInfo.notMatch.password;
+                  }
+                },
+              })}
+            />
+            {errors.cpassword && (
+              <ErrorMessage
+                message={
+                  errors.cpassword.message
+                    ? errors.cpassword.message
+                    : validationInfo.undefined
+                }
+              />
+            )}
+          </div>
         </div>
         <Button className="mt-6" type="submit" color="amber" fullWidth>
           Register
