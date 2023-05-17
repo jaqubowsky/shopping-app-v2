453148ed4eac7e58f32817f33bba7f2c3c3c657f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-17 22:56:43 +0200

    Clean code

--- src/pages/AddProduct/AddProductForm.tsx
+++ src/pages/AddProduct/AddProductForm.tsx
@@ -1,7 +1,4 @@
 import { onPromise } from "../../utils/onPromise";
-import { ErrorMessage } from "../../components/ErrorMessage";
-import { Input, Textarea } from "@material-tailwind/react";
-import SelectCategory from "./SelectCategory";
 import { FormInputs } from ".";
 import {
   Control,
@@ -10,6 +7,12 @@ import {
   UseFormRegister,
 } from "react-hook-form";
 import { FormEvent } from "react";
+import useLoginStatus from "../../hooks/useLoginStatus";
+import ContactSection from "./ContactSection";
+import PriceSection from "./PriceSection";
+import DescriptionSection from "./DescriptionSection";
+import ImageSection from "./ImageSection";
+import NameSection from "./NameSection";
 
 type AddProductFormProps = {
   control: Control<FormInputs>;
@@ -30,105 +33,23 @@ export default function AddProductForm({
   previewImage,
   onSubmit,
 }: AddProductFormProps) {
+  const { userData } = useLoginStatus();
+
   return (
     <form
       className="mb-2 mt-8 flex w-full flex-col gap-2"
       onSubmit={onPromise(handleSubmit(onSubmit))}
     >
-      <div className="mb-4 rounded-sm bg-gray-100 p-4">
-        <h2 className="mb-5 text-xl">The more details, the better!</h2>
-        <div className="flex w-3/4 flex-col gap-4">
-          <Input
-            size="lg"
-            label="Name"
-            color="amber"
-            {...register("name", {
-              required: "Product name is required.",
-            })}
-          />
-          {errors.name && (
-            <ErrorMessage
-              message={
-                errors.name.message ? errors.name.message : "Unknown error"
-              }
-            />
-          )}
-          <SelectCategory control={control} />
-        </div>
-      </div>
-      <div className="mb-4 flex flex-col gap-8 rounded-sm bg-gray-100 p-4">
-        <div className="mb-5">
-          <h2 className="text-xl">Photo</h2>
-          <h3 className="text-gray-600">Maximum file load is 5 MB!</h3>
-        </div>
-        <div className="relative flex flex-row justify-between">
-          <label
-            htmlFor="fileInput"
-            className="h-72 cursor-pointer rounded bg-yellow-700 p-4 text-center text-white hover:bg-yellow-600"
-          >
-            Choose File
-          </label>
-          {previewImage && (
-            <div className="h-72 w-72">
-              <img
-                id="preview"
-                className="h-full w-full object-cover"
-                src={previewImage}
-                alt="preview"
-              />
-            </div>
-          )}
-          <div className="gap-4">
-            <input
-              id="fileInput"
-              onInput={(e) => handleImageChange(e)}
-              type="file"
-              className="absolute inset-0 z-[-1] opacity-0"
-              {...register("image", {
-                required: "Image is required.",
-              })}
-            />
-            {errors.image && (
-              <ErrorMessage
-                message={
-                  errors.image.message ? errors.image.message : "Unknown error."
-                }
-              />
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="mb-4 flex flex-col gap-8 rounded-sm bg-gray-100 p-4">
-        <div className="w-3/4">
-          <h2 className="mb-5 text-xl">Description</h2>
-          <Textarea
-            color="amber"
-            variant="static"
-            placeholder="Enter the information that would be important to you when viewing such product."
-            {...register("description")}
-          />
-        </div>
-      </div>
-      <div className="mb-4 flex flex-col gap-8 rounded-sm bg-gray-100 p-4">
-        <div className="w-1/4">
-          <h2 className="mb-5 text-xl">Enter the price</h2>
-          <Input
-            color="amber"
-            label="Price"
-            icon="$"
-            {...register("price", {
-              required: "Price is required.",
-            })}
-          />
-          {errors.price && (
-            <ErrorMessage
-              message={
-                errors.price.message ? errors.price.message : "Unknown error"
-              }
-            />
-          )}
-        </div>
-      </div>
+      <NameSection register={register} errors={errors} control={control} />
+      <ImageSection
+        previewImage={previewImage}
+        handleImageChange={handleImageChange}
+        register={register}
+        errors={errors}
+      />
+      <DescriptionSection register={register} errors={errors} />
+      <PriceSection register={register} errors={errors} />
+      <ContactSection register={register} errors={errors} userData={userData} />
       <div className="text-center">
         <button className="main-button w-9/12">Add product!</button>
       </div>
