96979d44f58ecd39c0bebb99cefaef0709752744
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-17 22:55:19 +0200

    Create NameSection.tsx

---
+++ src/pages/AddProduct/NameSection.tsx
@@ -0,0 +1,40 @@
+import { Input } from "@material-tailwind/react";
+import { ErrorMessage } from "../../components/ErrorMessage";
+import { Control, FieldErrors, UseFormRegister } from "react-hook-form";
+import { FormInputs } from ".";
+import SelectCategorySection from "./SelectCategorySection";
+import { validationInfo } from "./validationInfo";
+
+type NameSectionProps = {
+  register: UseFormRegister<FormInputs>;
+  errors: FieldErrors<FormInputs>;
+  control: Control<FormInputs>;
+};
+
+function NameSection({ register, errors, control }: NameSectionProps) {
+  return (
+    <div className="mb-4 rounded-sm bg-gray-100 p-4">
+      <h2 className="mb-5 text-xl">The more details, the better!</h2>
+      <div className="flex w-3/4 flex-col gap-4">
+        <Input
+          size="lg"
+          label="Name"
+          color="amber"
+          {...register("name", {
+            required: validationInfo.required.name,
+          })}
+        />
+        {errors.name && (
+          <ErrorMessage
+            message={
+              errors.name.message ? errors.name.message : validationInfo.undefined
+            }
+          />
+        )}
+        <SelectCategorySection control={control} />
+      </div>
+    </div>
+  );
+}
+
+export default NameSection;
