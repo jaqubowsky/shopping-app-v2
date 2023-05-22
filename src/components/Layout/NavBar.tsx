ffd8af2a35dc0bb2902543c49c03ffa05ce0c585
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-22 12:20:53 +0200

    Clean code

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren} from "react";
 
 function NavBar({ children }: PropsWithChildren) {
 
   return (
-    <nav className="mx-auto flex w-11/12 items-center justify-around">
+    <nav
+      className="mx-auto flex w-11/12 items-center justify-around"
+    >
       <div className="flex items-center gap-6">
         <Link className="flex text-5xl font-black" to=".">
           Fake <span className="text-white drop-shadow-md">Store</span>
