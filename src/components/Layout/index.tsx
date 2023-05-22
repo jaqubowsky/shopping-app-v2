727412afd807627434340bce9727b8f7d584f7bf
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-22 12:21:22 +0200

    Hide header on scroll

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CartModal from "../../pages/Cart/CartModal";
 import UserIcon from "./UserIcon";
 import UserOptionsModal from "../../pages/Login/UserOptionsModal";
@@ -10,29 +10,48 @@ import AddProductIcon from "./AddProductIcon";
 export default function Layout() {
   const [showCart, setShowCart] = useState(false);
   const [showLoginDropdown, setShowLoginDropdown] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [showNavBar, setShowNavBar] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      setShowNavBar(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
   const toggleCart = () => {
-    setShowCart((prevState) => !prevState);
+    setShowCart(!showCart);
   };
 
   const toggleLoginDropdown = () => {
-    setShowLoginDropdown((prevState) => !prevState);
+    setShowLoginDropdown(!showLoginDropdown);
   };
 
   return (
     <>
-      <header className="bg-yellow-600 p-6">
+      <header
+        className={`fixed w-full bg-yellow-600 p-6 transition-all duration-300 ${
+          showNavBar ? "top-0" : "-top-48"
+        }`}
+      >
         <NavBar>
           <CartIcon toggleCart={toggleCart} />
           <UserIcon toggleLoginDropdown={toggleLoginDropdown} />
           <AddProductIcon />
         </NavBar>
       </header>
-
       <main className="mx-auto my-6 flex w-11/12 items-center justify-center">
         <Outlet />
       </main>
-
       <UserOptionsModal
         toggleLoginDropdown={toggleLoginDropdown}
         showLoginDropdown={showLoginDropdown}
