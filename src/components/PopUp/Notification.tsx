3eb6300b0ae0367e3f5b8667f85d1573ae3b7799
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-25 16:34:37 +0200

    Create Notification.tsx

---
+++ src/components/PopUp/Notification.tsx
@@ -0,0 +1,43 @@
+import { toast, ToastContainer } from "react-toastify";
+
+type NotifyProps = {
+  message: string;
+  type?: "success" | "error" | "warning";
+};
+
+export const notify = ({ message, type }: NotifyProps) => {
+  switch (type) {
+    case "success":
+      toast.success(message);
+      break;
+    case "error":
+      toast.error(message);
+      break;
+    case "warning":
+      toast.warn(message);
+      break;
+    default:
+      toast(message);
+  }
+};
+
+const Notification = () => {
+  return (
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+};
+
+export default Notification;
