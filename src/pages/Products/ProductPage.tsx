c85744826fc049552337838cd182cb29c95c6ea2
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-28 22:10:06 +0200

    Restyle product page and add RWD

--- src/pages/Products/ProductPage.tsx
+++ src/pages/Products/ProductPage.tsx
@@ -41,62 +41,68 @@ function ProductPage() {
   if (!product) return <div>Product not found</div>;
 
   return (
-    <div className="flex flex-col items-start justify-center gap-6">
+    <div className="flex w-full max-w-screen-xl flex-col items-center justify-center gap-6">
       {/* image */}
-      <section className="max-w-sm">
-        <img
-          className="h-full w-full object-cover"
-          alt={product.name}
-          src={product.imageUrl}
-        />
-      </section>
+      <div className="flex flex-col gap-4 md:flex-row">
+        <section className="product-section w-96">
+          <img
+            className="object-fit h-full w-full"
+            alt={product.name}
+            src={product.imageUrl}
+          />
+        </section>
 
-      {/* name */}
-      <section className="border border-b-gray-400 bg-gray-200 px-2 py-4">
-        <h2 className="mb-2 text-xl">{product.name}</h2>
-        <p className="mb-2 text-2xl font-bold italic">${product.price}</p>
-      </section>
+        {/* name */}
+        <div className="flex flex-col gap-4">
+          <section className="product-section h-1/4 max-w-sm">
+            <h2 className="mb-2 break-all text-xl">{product.name}</h2>
+            <p className="mb-2 text-2xl font-bold italic">${product.price}</p>
+          </section>
+          
+          {/* contact */}
+          <section className="product-section h-3/4">
+            <span>Private person</span>
+            <div className="my-4 flex items-center justify-start gap-2">
+              <img
+                className="h-12 w-12"
+                src={userData?.user.imageUrl}
+                alt={userData?.user.username}
+              />
+              <h2 className="text-2xl font-bold">{userData?.user.username}</h2>
+            </div>
+            <div className="flex flex-col gap-4 text-center">
+              <div className="flex gap-1">
+                <button className="main-button w-1/2">Call</button>
+                <button className="main-button-reverse w-1/2">
+                  Send Message
+                </button>
+              </div>
+              <span className="cursor-pointer text-sm text-gray-700 transition-colors hover:text-gray-900 hover:drop-shadow-md">
+                Get more offers from this seller
+              </span>
+            </div>
+          </section>
+        </div>
+      </div>
 
       {/* description */}
-      <section className="border border-b-gray-400 bg-gray-200 px-2 py-4">
+      <section className="product-section md:w-3/4">
         <h2 className="mb-2 text-xl">Description</h2>
         <p className="mb-2">{product.description}</p>
       </section>
 
-      {/* contact */}
-      <section className="border border-b-gray-400 bg-gray-200 px-2 py-4">
-        <span>Private person</span>
-        <div className="my-4 flex items-center justify-start gap-2">
-          <img
-            className="h-12 w-12"
-            src={userData?.user.imageUrl}
-            alt={userData?.user.username}
-          />
-          <h2 className="text-2xl font-bold">{userData?.user.username}</h2>
-        </div>
-        <div className="flex flex-col gap-4 text-center">
-          <div className="flex gap-1">
-            <button className="main-button w-1/2">Call</button>
-            <button className="main-button-reverse w-1/2">Send Message</button>
-          </div>
-          <span className="cursor-pointer text-sm text-gray-700 transition-colors hover:text-gray-900 hover:drop-shadow-md">
-            Get more offers from this seller
-          </span>
-        </div>
-      </section>
-
       {/* location */}
-      <section className="border border-b-gray-400 bg-gray-200 p-2">
-        <div className="flex flex-col justify-center gap-2">
+      <section className="product-section">
+        <div className="flex flex-col items-center justify-center gap-2">
           <a
-            className="cursor-pointer mb-2"
+            className="mb-2 cursor-pointer hover:drop-shadow-md"
             href={`https://www.google.pl/maps/place/${product.location}`}
             target="_blank"
             rel="noreferrer"
           >
             <img className="object-cover" src={imgUrl} alt={product.location} />
           </a>
-          <div className="flex gap-2 justify-center">
+          <div className="flex justify-center gap-2">
             <GoLocation className="text-2xl" />
             <p className="text-center">{product.location}</p>
           </div>
