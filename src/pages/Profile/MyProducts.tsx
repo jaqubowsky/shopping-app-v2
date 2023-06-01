c1f5c2c3874f016c50395f62e492980cd065db68
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-01 23:33:13 +0200

    Clean code

--- src/pages/Profile/MyProducts.tsx
+++ src/pages/Profile/MyProducts.tsx
@@ -74,7 +74,7 @@ export default function MyProducts() {
     <div className="flex flex-col items-center justify-center">
       <SearchBar
         clearSearchParams={clearSearchParams}
-        searchParams={searchParams.get("search")}
+        searchParams={searchParams}
         placeholder="Search your products..."
       />
       <div className="mt-8 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
