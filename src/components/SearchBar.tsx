7e67551d79c991230cbf7a73dfecfae0c38dbb6a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-01 23:31:11 +0200

    Add SearchBar logic

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,23 +1,58 @@
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 type SearchBarProps = {
-  placeholder: string;
+  placeholder?: string;
+  searchParams: URLSearchParams;
+  clearSearchParams?: () => void;
 };
 
-export default function SearchBar({ placeholder }: SearchBarProps) {
-  const { register } = useForm();
+export default function SearchBar({
+  placeholder = "Search...",
+  searchParams,
+  clearSearchParams,
+}: SearchBarProps) {
+  const { register, watch, reset } = useForm();
+  const navigate = useNavigate();
+
+  const searchParam = searchParams.get("search");
+  const categoryParam = searchParams.get("category");
+
+  const redirectPage = `/products?${
+    //eslint-disable-next-line
+    watch("search") ? `search=${watch("search")}&` : ""
+  }${categoryParam ? `category=${categoryParam}` : ""}`;
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    navigate(redirectPage);
+    reset();
+  }
 
   return (
-    <form className="flex justify-center">
-      <input
-        className="search-cancel::text-gray-600 rounded-sm border border-gray-400 p-2 outline-none focus:border-b-2 w-7/12"
-        type="search"
-        placeholder={placeholder}
-        {...register("search")}
-      />
-      <button className="rounded-sm bg-yellow-800 font-bold text-white transition-all hover:bg-yellow-700 px-8 py-2">
-        SEARCH
-      </button>
-    </form>
+    <div className="flex w-full max-w-screen-lg flex-col p-2">
+      <form className="mb-4 flex" onSubmit={onSubmit}>
+        <input
+          className="search-cancel::text-gray-600 w-full rounded-sm border border-gray-400 p-2 outline-none focus:border-b-2"
+          type="search"
+          placeholder={placeholder}
+          {...register("search")}
+        />
+        <button
+          type="submit"
+          className="rounded-sm bg-yellow-800 px-8 py-2 font-bold text-white transition-all hover:bg-yellow-700"
+        >
+          SEARCH
+        </button>
+      </form>
+      {!searchParam ? null : (
+        <button
+          onClick={clearSearchParams}
+          className="cursor-pointer self-start rounded-full bg-gray-300 p-2 text-gray-700 opacity-75 transition-all hover:bg-red-500 hover:text-white"
+        >
+          {searchParam}
+        </button>
+      )}
+    </div>
   );
 }
