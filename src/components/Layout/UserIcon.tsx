c6ae09717ab57c2cb130258a1b5b84975dbcab3a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-03 15:08:19 +0200

    SEO Optimization

--- src/components/Layout/UserIcon.tsx
+++ src/components/Layout/UserIcon.tsx
@@ -19,6 +19,7 @@ function UserIcon({ toggleLoginDropdown, userData }: UserIconProps) {
     <div
       tabIndex={0}
       role="button"
+      aria-label="user icon"
       onKeyDown={handleEnterPress}
       className="relative flex h-14 w-14 cursor-pointer items-center justify-center rounded-full bg-white p-4 text-center shadow-md"
       onClick={(e) => {
