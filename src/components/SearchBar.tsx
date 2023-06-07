8b3d931830ed65ccf29c30b1e01bf08594b3299a
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-08 01:24:25 +0200

    Fix SearchBar and clean code

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,74 +1,66 @@
-import { useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import SelectCategorySection from "../pages/ProductForm/SelectCategorySection";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { SearchBarForm } from "../types/form";
+import SelectCategorySection from "../pages/ProductForm/SelectCategorySection";
 import { Input } from "@material-tailwind/react";
+import { useEffect } from "react";
 
 type SearchBarProps = {
   placeholder?: string;
-  redirect: string;
   searchParams: URLSearchParams;
+  redirectTo?: string;
 };
 
 export default function SearchBar({
   placeholder = "Search...",
   searchParams,
-  redirect,
+  redirectTo = "/products",
 }: SearchBarProps) {
-  const { register, watch, reset, control } = useForm<SearchBarForm>();
+  const { control, reset, register, getValues } = useForm<SearchBarForm>();
   const navigate = useNavigate();
 
-  const searchParam = searchParams.get("search");
-  const categoryParam = searchParams.get("category");
-  const category = watch("category");
+  const clearSearchParams = () => {
+    searchParams.delete("search");
+    reset({ search: "", category: getValues("category") });
 
-  const createRedirectPage = useCallback(() => {
-    const currentSearch = watch("search") || searchParam || "";
-    const currentCategory = watch("category") || categoryParam || "";
-
-    return `${redirect}?search=${currentSearch}&category=${currentCategory}`;
-  }, [watch, searchParam, categoryParam, redirect]);
+    const queryString = searchParams.toString();
+    navigate(`${redirectTo}/?${queryString}`);
+  };
 
   useEffect(() => {
-    if (watch("category")) {
-      const redirectPage = createRedirectPage();
-      navigate(redirectPage);
-    }
-  }, [category, navigate, watch, createRedirectPage]);
+    const search = searchParams.get("search");
+    const category = searchParams.get("category");
 
-  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    reset({ search: search || "", category: category || "" });
+  }, [searchParams, reset, navigate, getValues]);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    navigate(createRedirectPage());
+    searchParams.set("search", getValues("search"));
+    searchParams.set("category", getValues("category"));
 
-    reset();
-  }
-
-  const clearSearchParams = () => {
-    const newSearchParams = new URLSearchParams(window.location.search);
-    newSearchParams.delete("search");
-
-    const redirectPage = `?${newSearchParams.toString()}`;
-
-    navigate(redirectPage);
+    const queryString = searchParams.toString();
+    navigate(`${redirectTo}/?${queryString}`);
   };
 
   return (
     <div className="flex flex-col p-2">
-      <form className="mb-4 grid gap-2 md:gap-0 grid-rows md:grid-cols-3 lg:grid-cols-5" onSubmit={onSubmit}>
+      <form
+        className="grid-rows mb-4 grid gap-2 md:grid-cols-3 md:gap-0 lg:grid-cols-5"
+        onSubmit={onSubmit}
+      >
         <div className="lg:col-span-3">
           <Input
             color="amber"
             aria-label="Search"
             className="search-cancel::text-gray-600 w-full rounded-sm border border-gray-400 p-2 outline-none focus:border-b-2"
             type="search"
-            label={placeholder}
             {...register("search")}
+            label={placeholder}
           />
         </div>
         <SelectCategorySection<SearchBarForm>
-          value={categoryParam || ""}
           control={control}
         />
         <button
@@ -78,12 +70,12 @@ export default function SearchBar({
           SEARCH
         </button>
       </form>
-      {!searchParam ? null : (
+      {!searchParams.get("search") ? null : (
         <button
           onClick={clearSearchParams}
           className="cursor-pointer self-start bg-gray-300 p-2 text-gray-700 opacity-75 transition-all hover:bg-red-500 hover:text-white"
         >
-          {searchParam}
+          {searchParams.get("search")}
         </button>
       )}
     </div>
