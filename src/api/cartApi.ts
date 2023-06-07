96d1ed1b4cc6670aa6c4f2e14189c4c1c242ea69
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-08 01:23:11 +0200

    Create cartApi.ts

---
+++ src/api/cartApi.ts
@@ -0,0 +1,50 @@
+import axios, { AxiosResponse } from "axios";
+import { Product } from "../types/product";
+const API_URL = "http://localhost:3001/api/";
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export const addToCart = async (productId: string) => {
+  try {
+    return await api.post("/cart", { productId: productId });
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      // eslint-disable-next-line
+      throw new Error(err.response?.data);
+    } else {
+      throw new Error("Unexpected error");
+    }
+  }
+};
+
+export const removeFromCart = async (productId: string) => {
+  try {
+    return await api.delete(`/cart/${productId}`);
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      // eslint-disable-next-line
+      throw new Error(err.response?.data);
+    } else {
+      throw new Error("Unexpected error");
+    }
+  }
+};
+
+export const getCartItems = async (): Promise<Product[]> => {
+  try {
+    const response: AxiosResponse<Product[]> = await api.get(`/cart`);
+    
+    return response.data;
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      console.log(err)
+      // eslint-disable-next-line
+      throw new Error(err.response?.data);
+    } else {
+      throw new Error("Unexpected error");
+    }
+  }
+};
