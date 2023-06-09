dd08094ddc3f2afd20624bac8f7224906db2775f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-09 21:41:25 +0200

    Clean code and add deleteAccount function

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,10 +1,16 @@
 import axios, { AxiosResponse } from "axios";
 import { LoginValues, RegisterValues, UserResponse } from "../types/user";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "http://localhost:3001/api/";
+const ALL_URL = "http://localhost:3001";
 
 const api = axios.create({
   baseURL: API_URL,
+  withCredentials: true,
+});
+
+const all = axios.create({
+  baseURL: ALL_URL,
   withCredentials: true,
 });
 
@@ -12,7 +18,7 @@ export const registerWithEmailAndPassword = async (
   registerValues: RegisterValues
 ) => {
   try {
-    await api.post("/register", registerValues);
+    await all.post("/register", registerValues);
   } catch (err: unknown) {
     if (axios.isAxiosError(err)) {
       // eslint-disable-next-line
@@ -25,7 +31,7 @@ export const registerWithEmailAndPassword = async (
 
 export const loginWithEmailAndPassword = async (formValues: LoginValues) => {
   try {
-    await api.post("/login", formValues);
+    await all.post("/login", formValues);
   } catch (err: unknown) {
     if (axios.isAxiosError(err)) {
       // eslint-disable-next-line
@@ -38,7 +44,7 @@ export const loginWithEmailAndPassword = async (formValues: LoginValues) => {
 
 export const checkLoginStatus = async (): Promise<UserResponse> => {
   try {
-    const response: AxiosResponse<UserResponse> = await api.get("/logged-in");
+    const response: AxiosResponse<UserResponse> = await all.get("/logged-in");
     const userData = response.data;
     return userData;
   } catch (err: unknown) {
@@ -52,12 +58,25 @@ export const checkLoginStatus = async (): Promise<UserResponse> => {
   }
 };
 
+export const deleteAccount = async (userId: string) => {
+  try {
+    return await api.delete(`/user/${userId}`);
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      // eslint-disable-next-line
+      throw new Error(err.response?.data);
+    } else {
+      throw new Error("Unexpected error");
+    }
+  }
+};
+
 export const getUserById = async (
   id: string | undefined
 ): Promise<UserResponse> => {
   try {
     if (id) {
-      const response: AxiosResponse<UserResponse> = await api.get(
+      const response: AxiosResponse<UserResponse> = await all.get(
         `/user/${id}`
       );
       const userData = response.data;
