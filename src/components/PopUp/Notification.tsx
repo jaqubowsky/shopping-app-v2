d5f1a2deb89604dc37f63195fb31bb739ba51dd9
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-09 21:40:54 +0200

    Add info notification

--- src/components/PopUp/Notification.tsx
+++ src/components/PopUp/Notification.tsx
@@ -2,13 +2,16 @@ import { toast, ToastContainer } from "react-toastify";
 
 type NotifyProps = {
   message: string;
-  type?: "success" | "error" | "warning";
+  type?: "success" | "error" | "warning" | "info";
 };
 
 export const notify = ({ message, type }: NotifyProps) => {
   switch (type) {
     case "success":
       toast.success(message);
+      break;
+    case "info":
+      toast.info(message);
       break;
     case "error":
       toast.error(message);
