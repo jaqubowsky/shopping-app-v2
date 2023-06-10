d6ba04f135000dda4dcdfb459a0907cbc00972e1
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-10 16:04:37 +0200

    Clean code

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -7,12 +7,12 @@ function NavBar({ children }: PropsWithChildren) {
 
   return (
     <nav className="mx-auto flex w-11/12 flex-col items-center justify-around gap-2 md:flex-row">
-      <div className="flex items-center gap-6 hover:scale-105 transition-all">
+      <div className="flex items-center gap-6 transition-all hover:scale-105">
         <Link className="flex text-5xl font-black text-gray-800" to="." replace>
           Fake <span className="text-white drop-shadow-md">Store</span>
         </Link>
       </div>
-      {!isMobile && <BreadcrumbsComponent /> }
+      {!isMobile && <BreadcrumbsComponent />}
       <div className="hover flex items-center justify-center gap-6 text-lg">
         <section className="flex gap-6">{children}</section>
       </div>
